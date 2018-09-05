cfff68d1f72977392d5aa89b030a2b64bb84e85c
zsharief <zsharief95@gmail.com>
2018-09-05 15:48:44 +0530

    added code from https://machinelearningmastery.com/statistics-for-machine-learning-mini-course/

---
+++ statistics-for-machine-learning-mini-course.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 13:00:44 2018
+
+@author: zahid
+"""
+
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from numpy import var
+from numpy import std
+
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(10000) + 50
+# calculate statistics
+print('Mean: %.3f' % mean(data))
+print('Variance: %.3f' % var(data))
+print('Std dev: %.3f' % std(data))
+
+
+## L4
+# calculate correlation coefficient
+from numpy.random import seed
+from numpy.random import randn
+from scipy.stats import pearsonr
+seed(1)
+data1 = 20 * rand(1000) + 100
+data2 = data1 + (10 * randn(1000) + 50)
+# calculate Pearson's correlation
+corr, p = pearsonr(data1, data2)
+print('Pearsons  correlation: %.3f' % corr)
