46a5a8d2268828465ab33a31ca0f48f6e2b1cd19
zsharief <30398916+zsharief@users.noreply.github.com>
2018-09-06 09:38:17 +0530

    modified roc-curves-and-precision-recall-curves-for-classification-in-python.py

--- roc-curves-and-precision-recall-curves-for-classification-in-python.py
+++ roc-curves-and-precision-recall-curves-for-classification-in-python.py
@@ -10,3 +10,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 from matplotlib import pyplot
+
+# generate a 2 class dataset
+X, y = make_classification(n_samples=1000, n_classes=2, weights=[1,1], random_state=1)
+# split into train and test sets
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2)
+
+# fit a model
