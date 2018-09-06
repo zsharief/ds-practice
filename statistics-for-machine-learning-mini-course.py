22eaca68e5acfadf2cd06fe54c0d493370ed5ef5
zsharief <zsharief95@gmail.com>
2018-09-06 18:45:46 +0530

    added code in statistics-for-machine-learning-mini-course.py

--- statistics-for-machine-learning-mini-course.py
+++ statistics-for-machine-learning-mini-course.py
@@ -32,3 +32,50 @@ data2 = data1 + (10 * randn(1000) + 50)
 # calculate Pearson's correlation
 corr, p = pearsonr(data1, data2)
 print('Pearsons  correlation: %.3f' % corr)
+
+
+## L5
+# student's t-test
+from numpy.random import seed
+from numpy.random import randn
+from scipy.stats import ttest_ind
+seed(1)
+# generate two independent samples
+data1 = 5 * randn(100) + 50
+data2 = 5 * randn(100) + 51
+# compare samples
+stat, p = ttest_ind(data1, data2)
+print('Statistics = %.3f, p = %.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+    print('Same distributions (fail to reject H0)')
+else:
+    print('Different distributions (reject H0)')
+
+
+## L6
+# calculate the confidence interaval
+from statsmodels.stats.proportion import proportion_confint
+# calculate the interval
+lower, upper = proportion_confint(88,100, 0.05)
+print('lower = %.3f,  upper = %.3f' % (lower, upper))
+
+
+## L7
+# example of mann-whitney u test
+from numpy.random import seed
+from numpy.random import rand
+from scipy.stats import mannwhitneyu
+seed(1)
+data1 = 50 + (rand(100) * 10)
+data2 = 51 + (rand(100) * 10)
+# compare samples
+stat, p = mannwhitneyu(data1, data2)
+print('Statistic = %.3f, p = %.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+    print('Same distributions (fail to reject H0)')
+else:
+    print('Different distributions (reject H0)')
